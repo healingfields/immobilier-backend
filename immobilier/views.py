71bedfae21c74b1f0aeeb43e5c8ab6ff7f0fdbcb
healingfields <dihratene@gmail.com>
2022-08-12 18:38:50 +0100

    created the view for retrieving the immobilier list

--- immobilier/views.py
+++ immobilier/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import ImmobilierSerializer
+from .models import Immobilier
 
 # Create your views here.
+class ImmobilierList(APIView):
+    def get(self, request):
+        queryset = Immobilier.objects.all()
+        serializer = ImmobilierSerializer(queryset, many=True)
+        return Response(serializer.data)
