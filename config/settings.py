20fb03f909474a755a87ae430f7c222ca6e2d966
healingfields <dihratene@gmail.com>
2022-08-15 12:40:02 +0100

    set up project to work with mongoDb

--- config/settings.py
+++ config/settings.py
@@ -84,12 +84,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "djongo",
         "NAME": env("DATABASE_NAME"),
-        "USER": env("DATABASE_USER"),
-        "PASSWORD": env("DATABASE_PASS"),
         "HOST": env("DATABASE_HOST"),
-        "PORT": env("DATABASE_PORT"),
     }
 }
 
